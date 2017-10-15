d0d7e5de756efdb375aadb70144b6c677f0fd0b7
jeansberg <jeansberg@gmail.com>
2017-10-15 16:24:46 +0300

    Add first part of character creation form

--- chargen-ui/src/components/CharacterForm.js
+++ chargen-ui/src/components/CharacterForm.js
@@ -1,19 +1,19 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import MultipleChoice from './MultipleChoice';
 import FreeText from './FreeText';
 
 class CharacterForm extends Component {
-    constructor(props)
-    {
+    constructor(props) {
         super(props);
     }
 
-    render()
-    {
+    render() {
         return (
             <div>
-                <FreeText title="Name" name="Name"/>
-                <MultipleChoice title="Skills" url={this.props.url + 'skills'}/>
+                <FreeText title="Name" name="Name" />
+                <MultipleChoice title="Type" url={this.props.url + 'types'} />
+                <MultipleChoice title="Descriptor" url={this.props.url + 'descriptors'} />
+                <MultipleChoice title="Focus" url={this.props.url + 'foci'} />
             </div>
         );
     }
