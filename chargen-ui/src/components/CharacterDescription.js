cb80e5a9573a2b77aa809fe0550920bf602a7d3d
jeansberg <jeansberg@gmail.com>
2017-10-22 19:27:54 +0300

    Takes Table component into use for character sheet

--- chargen-ui/src/components/CharacterDescription.js
+++ chargen-ui/src/components/CharacterDescription.js
@@ -1,4 +1,11 @@
 import React, {Component} from 'react';
+import Table from './Table'
+
+function getDescription(name, type, focus, descriptor)
+{
+    return (type && focus && descriptor) && 
+    <h4>{name && name + ", a"} {descriptor} {type} who {focus}</h4>
+}
 
 class CharacterDescription extends Component {
     constructor(props) {
@@ -14,9 +21,33 @@ class CharacterDescription extends Component {
                 <div className="card">
                     <h4 className="card-header">Character Sheet</h4>
                     <div className="card-body">
-                        <h6 className="card-subtitle mb-2 text-muted"> </h6>
-                   {<h4>{name && name + ", a"} {descriptor} {type} who {focus}</h4>}
+                        <h6 className="card-subtitle mb-2 text-muted">Description</h6>
+                    {getDescription(name, type, focus, descriptor)}
+                        <Table 
+                            name="Statistics"
+                            headers={['Stat', 'Pool', 'Edge']} 
+                            items={[
+                            ['Might', 12, 1],
+                            ['Speed', 10, 1],
+                            ['Intellect', 7, 0]]}>
+                        </Table>
+                        <Table name="Skills"
+                            headers={['Skill', 'Level']} 
+                            items={[
+                            ['Swimming', 'Trained'],
+                            ['Social Interactions', 'Inability'],
+                            ]}>
+                        </Table>
+                        <Table name="Abilities"
+                            headers={['Ability', 'Type', 'Cost']} 
+                            items={[
+                            ['Hedge Magic', 'Action', '1 Intellect'],
+                            ['Practiced with Light Weapons', 'Enabler', ''],
+                            ]}>
+                        </Table>
+                       
                     </div>
+              
                 </div>
             </div>
         );
